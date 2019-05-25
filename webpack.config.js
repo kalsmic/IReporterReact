e9797c6176612a78da07d7cf10b7a2d54b799ab7
Arthur Kalule <arthur.kalule@andela.com>
2019-05-25 21:05:40 +0300

    Integrate Travis CI and Coveralls

--- webpack.config.js
+++ webpack.config.js
@@ -22,8 +22,7 @@ module.exports = {
   },
   plugins: [
     new HtmlWebPackPlugin({
-      template: "./public/index.html",
-      filename: "./index.html"
+      template: "./public/index.html"
     })
   ]
 };
