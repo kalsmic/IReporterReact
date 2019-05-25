8b6b7a758240e83b0daab5901ffaa614b317723f
Arthur Kalule <arthur.kalule@andela.com>
2019-05-25 19:55:12 +0300

    Configure testing environment

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from "react";
 
-export default class IReporterApp extends Component {
+class IReporterApp extends Component {
   render() {
     return (
-      <div>
+      <div data-test="welcomeHeader">
         <h1>Welcome to IReporter React</h1>
       </div>
     );
   }
 }
+
+export default IReporterApp;
