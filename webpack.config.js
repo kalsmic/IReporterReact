f5fdad363fcc6a30a38d93c9ac0cd608adf6001f
Arthur Kalule <arthur.kalule@andela.com>
2019-06-28 16:54:47 +0300

    chore(setup): heroku deployment

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,13 @@
 const HtmlWebPackPlugin = require("html-webpack-plugin");
+const path = require("path");
 
 module.exports = {
   entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "main.js",
+    publicPath: "/",
+  },
   module: {
     rules: [
       {
